613a1e8508be6343aaef064e9fa075fed0605e2f
ike <okaforikennapaul@gmail.com>
2025-11-03 12:26:42 +0100

    new feature

---
+++ loan/main/static/JS/main.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+  menuBtn.classList.toggle("rotate-90");
+});
+
+alert("hi");
